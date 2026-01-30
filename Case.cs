4716a8c88c77a3ac810f19f6c633ba55e22807bd
OG5001 <olahgergo917@gmail.com>
2026-01-30 09:30:55 +0100

    Ügy állapota készülőben.

--- Case.cs
+++ Case.cs
@@ -8,5 +8,26 @@ namespace DigitalisNyomozoIroda
 {
 	internal class Case
 	{
+		private string ugyAzonosito;
+		private string cim;
+		private string leiras;
+		private string allapot;
+
+
+		public Case(string ugyAzonosito, string cim, string leiras, string allapot)
+		{
+			this.ugyAzonosito = ugyAzonosito;
+			this.cim = cim;
+			this.leiras = leiras;
+			this.allapot = allapot;
+			List<Person> personLista = new List<Person>();
+			List<Evidence> bizonyitekLista = new List<Evidence>();
+		}
+
+		public string UgyAzonosito { get => ugyAzonosito; set => ugyAzonosito = value; }
+		public string Cim { get => cim; set => cim = value; }
+		public string Leiras { get => leiras; set => leiras = value; }
+		public string Allapot { get => allapot; set => allapot = value; }
+
 	}
 }
--- CaseStatus.cs
+++ CaseStatus.cs
@@ -8,5 +8,21 @@ namespace DigitalisNyomozoIroda
 {
 	internal class CaseStatus
 	{
+		private string aktualisStatusz;
+
+		public CaseStatus(string aktualisStatuszstatus)
+		{
+			aktualisStatusz = aktualisStatuszstatus;
+		}
+
+		public string Status { get => aktualisStatusz; set => aktualisStatusz = value; }
+
+		public string allapot(string s)
+		{
+			Console.WriteLine("");
+			string abc = Console.ReadLine();
+			aktualisStatusz = s;
+			return aktualisStatusz;
+		}
 	}
 }
