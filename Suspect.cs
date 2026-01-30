5f3002aee555d35b02af4e6607a287d652037b22
OG5001 <olahgergo917@gmail.com>
2026-01-30 09:53:18 +0100

    Kész a Suspect class objektumai.

--- Suspect.cs
+++ Suspect.cs
@@ -8,5 +8,24 @@ namespace DigitalisNyomozoIroda
 {
 	internal class Suspect
 	{
+		private Person person;
+		private int szint;
+		private string status;
+
+		public Suspect(Person person, int szint, string status)
+		{
+			this.person = person;
+			this.szint = szint;
+			this.status = status;
+		}
+
+		public int Szint { get => szint; set => szint = value; }
+		public string Status { get => status; set => status = value; }
+		internal Person Person { get => person; set => person = value; }
+
+		public override string ToString()
+		{
+			return $"Gyanusított Adatok: {this.Person.ToString}\nGyanusítottsági szint:{this.szint}\nStátusz: {this.status}";
+		}
 	}
 }
