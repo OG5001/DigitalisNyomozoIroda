0b6cf52c4e8d0528af0aa1450dcb4f6dd84b57ba
OG5001 <olahgergo917@gmail.com>
2026-01-30 09:08:25 +0100

    A User class befejezve.

--- User.cs
+++ User.cs
@@ -8,5 +8,19 @@ namespace DigitalisNyomozoIroda
 {
 	internal class User
 	{
+		private string nev;
+		private string azonosito;
+		private string szerepkor;
+
+		public User(string nev, string azonosito, string szerepkor)
+		{
+			this.nev = nev;
+			this.azonosito = azonosito;
+			this.szerepkor = szerepkor;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public string Azonosito { get => azonosito; set => azonosito = value; }
+		public string Szerepkor { get => szerepkor; set => szerepkor = value; }
 	}
 }
